a03879ae19aec891788254ed3a1df6f4da09d35e
Chet Kener <Cl3Kener@gmail.com>
2014-12-12 01:20:48 -0500

    Disable Lots of Debugging
    Revert "Debugging: Enable All Debugging until GPS bug is Squashed"
    
    This reverts commit a7e2e0bfc70c866645c2f45df2ab06030c7e1131.
    
    Conflicts:
    	policy/src/com/android/internal/policy/impl/keyguard/KeyguardServiceDelegate.java
    	telephony/java/android/telephony/SubscriptionManager.java

--- services/core/java/com/android/server/fingerprint/FingerprintService.java
+++ services/core/java/com/android/server/fingerprint/FingerprintService.java
@@ -58,7 +58,7 @@ import java.util.Map;
  */
 public class FingerprintService extends SystemService {
     private final String TAG = "FingerprintService";
-    private static final boolean DEBUG = true;
+    private static final boolean DEBUG = false;
     private ArrayMap<IBinder, ClientData> mClients = new ArrayMap<IBinder, ClientData>();
 
     private static final int MSG_NOTIFY = 10;
