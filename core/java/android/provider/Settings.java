b3f50c689700bfc0d17cdb7bed2804fae1728675
BigBrother1984 <stevewatersy@gmail.com>
2015-06-29 04:26:50 +0300

    Forward Port:Squahed Floating Window with Multitasking. Forward Port and Fixups done by BlackDragon && rogersb11
    Floating window contributors:
    drcmda
    beerbong
    D4rKn3sSyS
    
    This feature, has ability to:
    1. Moveable window to various position
    2. Scaling window with 2 finger like zoom in/out
    3. Double tap in actionbar or above window to fullscreen or back to floating window
    4. Automatically resize when Snapping into one of side (Top, Bottom, Left, Right)
       (taken from xhalofloatingwindow by zst123)
    5. Automatically switch focus from other app (taken from multiwindow by xplodwild)
    6. Unlimited floating window app (depends on RAM)
    
    To make app launch using Floating window feature
    
    you must add Intent.FLAG_FLOATING_WINDOW
    before start an activity
    
    for example you can look at https://gerrit.omnirom.org/6515
    
    need https://gerrit.omnirom.org/#/c/6561
    
    PS.17 : fix scaling
    PS.18 : fix layout calculation when rotate
    PS.19 : support minimize/maximize window (see App circle sidebar)
    PS.20 : fix previouse app
    PS.21 : rebased due to changes in 4.4.3_r1.1
    PS.26 : rebased
    PS.27 : rebased
    PS.30 : a little touch :)
    PS.31 : fix FCs because package manager has died
    
    Fusion:
    
    PS.32 : Adapted to work on lollipop.
    PS.33 : Kill floating activities running in the same thread when tapping outside.
    PS.34 : Recents: Launch as Floating App.
    PS.35 : Added some tinted code by XplodWild.
    PS.36 : Remove whitespace.
    PS.37 : Added floating window intent to slimactions.
    PS.38 : Cleanup unused code.
    
    Todo: add more intents for some features to allow open in floating window.
    
    Signed-off-by: BlackDragon <blackdragon.fusionteam@gmail.com>
    Signed-off-by: rogersb11 <brettrogers11@gmail.com>
    Slim actions floating mode switch 2/2
    
    Change-Id: I94c2f29bb4c920b6cf894b59d43f79ef659a5988
    
    Signed-off-by: rogersb11 <brettrogers11@gmail.com>
    
    Floating window update.
    
    Signed-off-by: BlackDragon <blackdragon.fusionteam@gmail.com>
    
    Additionl commits
    
    https://github.com/AICP/frameworks_base/commit/1b1f2b4e7aef63589038a1bbf4f45c23284310cc
    https://github.com/AICP/frameworks_base/commit/fcd0a3fdb20fd607ac25825dec83f540f1c85136
    https://github.com/AICP/frameworks_base/commit/c9e42ac9f1c61725fe37964de45082c78915a5aa
    https://github.com/AICP/frameworks_base/commit/60146b248584b0853558110dd5097fda44fb63e5
    https://github.com/AICP/frameworks_base/commit/fccc3c5c5886a6a0fe81efd738559dfaa48174f9
    https://github.com/AICP/frameworks_base/commit/31d31d702c3f2832d0c4ec2c4af0a011d66d1c0b
    https://github.com/AICP/frameworks_base/commit/89134dd39d6c168f894ab565e5262155c7cdb052
    https://github.com/AICP/frameworks_base/commit/23cca94f7e18b7427f50d5732d0b9dee024e2ca5
    
    MergeD from AICP
    
    Conflicts:
    	core/java/android/content/Intent.java
    	core/java/android/view/IWindowManager.aidl
    	core/java/com/android/internal/util/cm/QSConstants.java
    	core/res/res/values/rr_strings.xml
    	packages/CaptivePortalLogin/src/com/android/captiveportallogin/CaptivePortalLoginActivity.java
    	packages/SystemUI/res/values/rr_strings.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4577,6 +4577,12 @@ public final class Settings {
         public static final String NX_LONGSWIPE_THRESHOLD_DOWN_LAND = "nx_longswipe_threshold_down_land";      
 
         /**
+         * Option to launch slim actions in floating mode
+         * @hide
+         */
+        public static final String SLIM_ACTION_FLOATS = "slim_action_floats";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
